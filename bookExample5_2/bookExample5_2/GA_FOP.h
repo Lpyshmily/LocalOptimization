0484da59d59642c6a27043c2f61e1f8ff7f5f0fc
郭祥 <guox0405@163.com>
2020-07-02 12:46:09 +0800

    bookExample5_2 15th version

--- bookExample5_2/bookExample5_2/GA_FOP.h
+++ bookExample5_2/bookExample5_2/GA_FOP.h
@@ -5,6 +5,8 @@ int GA_FOP(double* Out, const double* rv0, const double* rv1, double m0, double 
 int GA_FOP_2(double* Out, const double* rv0, const double* rv1, double m0, double tof, double epsi, int MaxGuessNum, const double* rv_middle, double PSO_t);
 int GA_FOP_type1(double* Out, const double* rv0, const double* rv1, double m0, double tof, int MaxGuessNum, const double* rv_middle, double PSO_t);
 int GA_FOP_type2(double* Out, const double* rv0, const double* rv1, double m0, double tof, double epsi, int MaxGuessNum, const double* rv_middle, double PSO_t);
+int GA_FOP_type2_process(double* Out, const double* rv0, const double* rv1, double m0, double tof, double epsi, int MaxGuessNum, const double* rv_middle, double PSO_t);
 int change_epsi(double* Out, const double* rv0, const double* rv1, double m0, double tof, int MaxGuessNum, const double* rv_middle, double PSO_t);
+int change_epsi_type2(double* Out, const double* rv0, const double* rv1, double m0, double tof, int MaxGuessNum, const double* rv_middle, double PSO_t);
 
 #endif
